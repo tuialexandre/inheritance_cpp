fb1d9702af727eeb92ff412e6270b7f22a118d3f
Tui Baraniuk <tuialexandre@gmail.com>
2021-08-24 11:18:52 -0300

    Updated to smart pointer

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "cylinder.h"
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
 int main() {
     float base_radius = 0.5;
     float height = 2.0;
-    Polygon * my_circle = new Circle(base_radius);
+    unique_ptr<Polygon> my_circle = make_unique<Circle>(base_radius);
     Cylinder my_cylinder = Cylinder(height, base_radius);
 
     cout << "Circle area: "<< my_circle->area() << endl;
